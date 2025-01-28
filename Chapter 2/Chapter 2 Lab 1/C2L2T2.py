ab82ac7920075022b10282e76e77d04648ca5c5a
Adit Gautam <gautamadit@gmail.com>
2025-01-27 22:51:09 -0800

    Finished C2L1

---
+++ Chapter 2/Chapter 2 Lab 1/C2L2T2.py
@@ -0,0 +1,18 @@
+## Adit Gautam
+# 1/27/25
+# A program that performs basic operations given two integers
+
+#declare 2 integers to be used
+a = -4
+b = 4
+
+#Operations and print statements
+
+print("a = ", a, ", b = " ,b)
+print("Sum:", a + b)
+print("Difference", a - b)
+print("Product:", a * b)
+print("Average:", (a+b) / 2)
+print("Distance:", abs(a-b))
+print("Minimum:", min(a,b))
+print("Maximum:", max(a,b))
---
+++ Chapter 2/Chapter 2 Lab 1/type_test.py
@@ -0,0 +1,13 @@
+## Adit Gautam
+# 1/27/25
+# a program that demonstrates variables and different types of data
+
+
+# Testing different types in the same variable
+taxRate = 5  # int
+print(taxRate)
+taxRate = 5.5  # float
+print(taxRate)
+taxRate = "Non-taxable" # string
+print(taxRate)
+print(taxRate + '5')
